1e2ddf2f58c7f700028da858908bb0eb5eb18662
Petr Boček <petr.bocek13@gmail.com>
2024-09-22 21:11:26 +0200

    Bulls & Cows verze 02
    Doplneny funkce:
    request_users_nickname -> moznost ulozeni skore a vyzadani uzivatelskeho jmena
    save_score_csv -> ulozeni skore do souboru "savesbc.csv"
    retriene_saved_score -> nacteni ulozenych vysledku
    pick_top_five_scores -> vyfiltrovani peti nejlepsich vysledku
    print_top_five -> vypsani peti nejlepsich vysledku
    mereni delky hry

--- bulls_&_cows.py
+++ bulls_&_cows.py
@@ -6,27 +6,19 @@ email: bocek2@seznam.cz
 discord: Seth_Cz#8510
 """
 
+import os
+import sys
 import random
 import time
+import datetime
+import csv
+from operator import itemgetter
 
-# doplnit funkci na ukádání skóre do souboru
-# doplnit funkci na měření času
-#number_of_guess = 0
 
 def greet_user() -> str:
 
-    """Greet the user and print the introductory text."""
+    """Greets the user and print the introductory text."""
 
-#     print(
-#         f"""\
-# Hi there!
-# {"-" * 47}
-# I've genereted a random 4 digit number for you.
-# Let's play a bulls and cows game.
-# {"-" * 47}
-# Enter a number:
-# {"-" * 47}"""
-# )
     print(
         f"Hi there!\n"
         f"{"-" * 47}\n"
@@ -37,10 +29,15 @@ def greet_user() -> str:
         f"{"-" * 47}"
     )
 
+    return
+
 
 def generate_random_number() -> str:
 
-    """Generate a random four-digit number, every digit is unique."""
+    """
+    Generates a random four-digit number, where each digit is unique
+    and the number doesn't start with zero.
+    """
 
     random_number = str()
 
@@ -67,8 +64,9 @@ def request_users_number() -> str:
 def tests_users_number(users_number: str) -> str:
 
     """
-    Test whether the number entered by the user meets
+    Tests whether the number entered by the user meets
     the conditions of a suitable number for the game.
+    If not, func returns aprropriate warn.
     """
     warn = None
 
@@ -92,6 +90,7 @@ def tests_users_number(users_number: str) -> str:
 
 
 def evaluate_users_tip(random_number: str, users_number: str) -> tuple[int]:
+    
     """
     Compares the user's number with a randomly generated number
     and returns the number and type of matches.
@@ -114,6 +113,7 @@ def evaluate_users_tip(random_number: str, users_number: str) -> tuple[int]:
 
 
 def print_tip_result(bulls_cows: tuple[int], number_of_guess: int) -> str:
+    
     """
     Return result of user's tip according to type and number of correct matches.
     """
@@ -139,18 +139,120 @@ def print_tip_result(bulls_cows: tuple[int], number_of_guess: int) -> str:
     return result
 
 
-# def count_number_of_guess(bulls: int) -> int:
-#     """
-#     Returns the number of attempts it took the user 
-#     to guess the number.
-#     """
+def request_users_nickname(blank_nick: str ="") -> str:
 
-#     global number_of_guess
+    """
+    Gives the user the option to save the game score and asks for the user's nickname.
+    """
 
-#     if bulls <= 4:
-#         number_of_guess += 1
+    save = input("Do you want save your score? [Y/N] \n")
+    print(f"{"-" * 47}")
 
-#     return number_of_guess
+    if save.upper() == "Y":
+        nick_name = input("Enter your nickname\n(max 5 alphanumeric characters long): \n")
+        print(f"{"-" * 47}")
+        if len(nick_name) <= 5 and nick_name.isalnum():
+            return nick_name
+        else:
+            print(
+                f"Nickname has to be max 5 characters long!\n"
+                f"And all characters has to be alphanumeric only!"
+            )
+            print()
+
+    
+    elif save.upper() == "N":
+        print("Thanks for playing Bulls & Cows game. BB")
+        sys.exit()
+
+    else:
+        print("Options are only Y or N!")
+        print()
+        nick_name = ""
+
+    return request_users_nickname(blank_nick + nick_name)
+
+
+def save_score_csv(
+        nick_name: str,
+        number_of_guess: int,
+        final_time: str
+        ):
+    
+    """
+    Saves players score to '.csv' file 'savesbc.csv'
+    """                                                                          
+    
+    path_bc = os.path.realpath("savesbc.csv")
+    header = ("NICK", "GUESSES", "FINAL TIME")
+    player_stats = [nick_name, number_of_guess, final_time]
+    
+    if not os.path.exists(path_bc):
+        with open(path_bc, mode="w", encoding="utf-8", newline='') as saves_csv:
+            writer = csv.writer(saves_csv)
+            writer.writerows((header, player_stats))
+
+    else:
+        with open(path_bc, mode="a", encoding="utf-8", newline='') as saves_csv:
+            writer = csv.writer(saves_csv)
+            writer.writerow(player_stats)
+
+    return
+
+
+def retrieve_saved_score(saved_score_file: str = "savesbc.csv") -> tuple[list[str | int], ...] | str:
+
+    """
+    Returns all saved scores from the file savesbc.csv as a tuple.
+    If the file doesn't exist, prints an info line.
+    """
+
+    try:
+        with open(saved_score_file, mode="r", encoding="utf-8", newline='') as csv_save:
+            score_reader = csv.reader(csv_save)
+            return tuple(score_reader)
+
+    except FileNotFoundError:
+        print("File with saved score not found.")
+    
+
+def pick_top_five_scores(raw_scores_data: tuple[list[str | int], ...]) -> tuple[list[str | int], ...]:
+
+    """
+    Sorts scores data by number of guesses and final time and return five top scores.
+    """
+    ordered_scores_data = sorted(raw_scores_data[1:], key= itemgetter(1, 2))
+    #ordered_scores_data_lambda = sorted(test_tuple[1:], key= lambda tup: (tup[1], tup[2]))
+
+    if len(ordered_scores_data) > 5:
+        top_five = ordered_scores_data[:5]
+    else:
+        top_five = ordered_scores_data
+
+    top_five.insert(0, raw_scores_data[0])
+
+    return tuple(top_five)
+
+
+def print_top_five(top_five: tuple[list[str | int], ...]) -> str:
+
+    """
+    Prints the top five scores from the save file if available.
+    The printing of individual characters is delayed
+    to achieve the "typewriter" effect.
+    """
+
+    for row in top_five:
+        for record in row:
+            for char in record:
+                print(char, end="", flush=True); time.sleep(0.085)
+            print(f"\t{" ":^8}", end=""); time.sleep(0.35)
+        print(); time.sleep(0.35)
+            
+    print(f"{"-" * 47}")
+    print("Thanks for playing Bulls & Cows game. BB")
+    sys.exit()
+
 
 def main():
     
@@ -158,11 +260,12 @@ def main():
 
     random_number = generate_random_number()
     number_of_guess = 1
+    start = time.time()
 
     while True:
         
         users_number = request_users_number()
-        warn = tests_users_number(users_number)
+        warn = tests_users_number(users_number)        
 
         if warn != None:
             print(warn)
@@ -170,7 +273,6 @@ def main():
         
         else:
             bulls_cows = evaluate_users_tip(random_number, users_number)
-            #number_of_guess = count_number_of_guess(bulls_cows[0])
             print(print_tip_result(bulls_cows, number_of_guess))
             
             if bulls_cows[0] != 4:
@@ -178,8 +280,20 @@ def main():
                 continue
             
             else:
+                end = time.time()
+                formated_time = str(datetime.timedelta(seconds=round(end-start)))
+                print(f"It took you {formated_time}\n{"-" * 47}")   
                 break
-  
+    
+    nick_name = request_users_nickname()
+
+    save_score_csv(nick_name, number_of_guess, formated_time)
+
+    raw_scores_data = retrieve_saved_score()
+
+    top_five = pick_top_five_scores(raw_scores_data)
+
+    print_top_five(top_five)
 
 if __name__ == "__main__":
     main()
